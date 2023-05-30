f9e9c37aac218d4b6ed6cc32543771b8d7cf9fd6
ValentynYurchenko <valentin.urchenko@gmail.com>
2023-05-30 21:34:53 +0300

    map display

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
 
 // Использование Geolocation API
+// Отображение карты с использованием библиотеки Leaflet
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -17,6 +18,20 @@ if (navigator.geolocation) {
       console.log(
         `https://www.google.com/maps/@${latitude},${longitude},16z?hl=ru&entry=ttu`
       );
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Невозможно получить ваше местоположение!');
