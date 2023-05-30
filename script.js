3bdc91ee4feb1859354ed052ee0a0e7899909f19
ValentynYurchenko <valentin.urchenko@gmail.com>
2023-05-30 19:34:08 +0300

    determination of the current location

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+// Использование Geolocation API
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},16z?hl=ru&entry=ttu`
+      );
+    },
+    function () {
+      alert('Невозможно получить ваше местоположение!');
+    }
+  );
+}
